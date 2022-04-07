280d78f234d8544d996d1f372d198a4760615336
otienosteve <steveotieno701@gmail.com>
2022-04-07 22:54:30 +0300

    add a number of changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { GoalComponentComponent } from './goal-component/goal-component.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import {FormsModule} from '@angular/forms';
+import { GoalFormComponent } from './goal-form/goal-form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponentComponent,
+    GoalDetailComponent,
+    StrikethroughDirective,
+    GoalFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
